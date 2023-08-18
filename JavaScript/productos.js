f0c54c57fdcd77a06a60725173b36b4ab3e0802a
alanRodrigoMiozzi <alanks.moz@gmail.com>
2023-08-18 09:50:20 -0300

    entrega final del proyecto, hicimos una sola pagina para realizar compra

---
+++ JavaScript/productos.js
@@ -0,0 +1,42 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Onigiri",
+      precio: 200,
+      img:"https://www.sal-pimienta.com/wp-content/uploads/2020/05/Takoyaki-1-500x500.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Dumplings",
+      precio: 800,
+      img:
+        "https://t1.uc.ltmcdn.com/es/posts/9/5/9/como_hacer_dumplings_chinos_46959_orig.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Ebi Furai",
+      precio: 500,
+      img:
+        "https://www.thespruceeats.com/thmb/5xkGMB8ZXz3KGF_y4Uxf7ZfQAvQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ebi-fry-fried-shrimp-2031450-hero-01-46c436a89c164a9ab5980f888097fcd2.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Karaage",
+      precio: 700,
+      img:
+        "https://static01.nyt.com/images/2018/07/25/dining/HK-karaage-horizontal/merlin_141075300_74569dec-9fc2-4174-931d-019dddef3bb8-threeByTwoMediumAt2X.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 5,
+      nombre: "Harumaki",
+      precio: 300,
+      img:
+        "https://www.thespruceeats.com/thmb/eulPO__yn89VoKNCzSAdoa4A5fQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetable-harumaki-japanese-spring-rolls-2030917-step-20-a053639af7764ee0b9f091d1ab1bc364.jpg",
+      cantidad: 1,
+    },
+  ];
+  
